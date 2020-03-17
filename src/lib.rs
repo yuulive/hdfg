f1aa0818fcfbbf0798882558e9cfe471020691c7
Illia Chub <dragonhaze@ilchub.net>
2020-03-17 17:08:18 +0200

    minor changes

--- src/lib.rs
+++ src/lib.rs
@@ -17,18 +17,18 @@ pub fn find_blin_amount(mut flour_amount: f32, mut milk_amount: f32, mut eggs_am
     let smallest: f32;
     if flour_amount<=milk_amount && flour_amount <=eggs_amount as f32{
         smallest = flour_amount as f32;
-        return flour_amount * 6.0;
+        return smallest * 6.0;
     }
     else if milk_amount<=flour_amount && milk_amount <=eggs_amount as f32 {
         smallest = milk_amount as f32;
-        return milk_amount * 6.0;
+        return smallest * 6.0;
     }
     else if eggs_amount as f32<=flour_amount && eggs_amount as f32<=milk_amount {
         smallest = eggs_amount as f32;
-        return eggs_amount as f32 * 6.0;
+        return smallest as f32 * 6.0;
     }
     else{
-        return -1.0;
+        return -1 as f32;
 }
 }
 pub fn find_materials_amount(mut flour_amount: f32, mut milk_amount: f32, mut eggs_amount: i32) -> (f32,f32,i32) {
